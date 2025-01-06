3916ddf997b15ca68308ffe58e41951a6ba97c27
Vaidik <vaidik07@gmail.com>
2025-01-06 11:23:56 +0530

    Filter component done

--- wudlux/frontend/src/App.jsx
+++ wudlux/frontend/src/App.jsx
@@ -15,11 +15,8 @@ import LoginPage from "./Pages/Login/Login";
 import RegistrationForm from "./Pages/Register/Register";
 import Productlist from "./Components/ProductList/Productlist";
 import ForgotPassword from "./Pages/ForgetPassword/ForgetPassword";
-<<<<<<< HEAD
 import Checkout from "./Pages/Checkout/Checkout";
-=======
 import ProductInfo from "./Pages/ProductInfo/ProductInfo";
->>>>>>> 123652bcc80c7a2b2edfd0fa5b8bc383a168fe7f
 
 function App() {
   return (
@@ -50,11 +47,8 @@ function App() {
         <Route path="/log-in" element={<LoginPage />} />
         <Route path="/sign-up" element={<RegistrationForm />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-<<<<<<< HEAD
         <Route path="/checkout" element={<Checkout />} />
-=======
         <Route path="/product-info" element={<ProductInfo />} />
->>>>>>> 123652bcc80c7a2b2edfd0fa5b8bc383a168fe7f
       </Routes>
       <Footer />
     </>
